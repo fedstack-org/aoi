64aa42ee4c2c7cd697ea3ce535bfddd4e578ce61
thezzisu <thezzisu@gmail.com>
2026-01-08 16:45:59 +0800

    fix: add flag to control the new behavior & add yarn version strategy

--- apps/frontend/src/utils/flags.ts
+++ apps/frontend/src/utils/flags.ts
@@ -7,3 +7,4 @@ export const loginHint = import.meta.env.VITE_LOGIN_HINT
 export const verifyHint = import.meta.env.VITE_VERIFY_HINT
 export const enableOverview = !!import.meta.env.VITE_ENABLE_OVERVIEW
 export const enableSlugFinder = !!import.meta.env.VITE_ENABLE_SLUG_FINDER
+export const enableLoginAutoRedirect = !!import.meta.env.VITE_ENABLE_LOGIN_AUTO_REDIRECT
